c17e6ff1eacadc76e9457bac5fdc876af17106de
Essen Roman <essenr@mail.ru>
2023-08-13 13:48:15 +0400

    Add new task 10 13 15

---
+++ task10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.WriteLine("Task 10 ");
+Console.Write("Write three-digit number: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("Second digit of this number -> "+stringNumber[1]);
---
+++ task15/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Write nunber, day of week: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("(This is weekend) -> yes");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("this is not day of week");
+  }
+  else Console.WriteLine("(This day is not weekend) -> no");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
