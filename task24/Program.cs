750749122a163bdd4a5ef033d78404d0f95aea3c
Essen Roman <essenr@mail.ru>
2023-09-12 20:06:57 +0400

    new tasks

---
+++ task24/Program.cs
@@ -0,0 +1,25 @@
+﻿int SumNumbers(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            sum += i; // sum = sum + i
+        }
+    }
+    return sum;
+}
+
+Console.WriteLine("Write number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumNumbers = SumNumbers(number);
+
+if(number < 1)
+{
+    Console.WriteLine("Incorrect");
+    return;
+}
+
+Console.WriteLine($"Sum numbers {number} = {sumNumbers}");
---
+++ task26/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+int DigitCount(int num)
+{
+    int count = 0;
+    if(num == 0) return 1;
+    
+    while(num != 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+
+Console.Write("Write number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Number {number} comprises {DigitCount(number)} signs.");
+
